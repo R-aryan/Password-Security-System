9f419ab940150675392af8ed5b47e63bb47e198c
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2017-09-11 19:34:26 +0530

    Added Comments

--- Psssword_Security_System/PS_SHA_256.h
+++ Psssword_Security_System/PS_SHA_256.h
@@ -12,6 +12,23 @@
 typedef unsigned char uc;             // 8-bit uc
 typedef unsigned int  ui;             // 32-bit ui, change to "long" for 16-bit machines
 
+/*
+The unsigned char simply means: Use the most significant bit instead of treating it as a bit flag for +/- sign
+ when performing arithmetic operations.
+It makes significance if you use char as a number for instance:
+
+typedef char BYTE1;
+typedef unsigned char BYTE2;
+
+BYTE1 a;
+BYTE2 b;
+For variable a, only 7 bits are available and its range is (-127 to 127) = (+/-)2^7 -1.
+For variable b all 8 bits are available and the range is 0 to 255 (2^8 -1).
+
+If you use char as character, "unsigned" is completely ignored by the compiler just as comments are removed from your program.
+
+*/
+
 typedef struct {
 	uc data[64];
 	ui datalen;
